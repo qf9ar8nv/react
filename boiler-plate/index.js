49c360f65ca557f5193778a3070a51468cd55c41
qf9ar8nv <ckguscjf0@gmail.com>
2021-02-03 23:05:32 +0900

    Add additonal setting
    * mongodb랑 node.js 연결.
    * 포스트맨을 통해 sign-in 구조를 만듬.

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,9 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const { User } = require("./models/User");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+//application/x-www-form-urlencoded -> 이렇게 된 데이터를 분석해서 가져올 수 있게함.
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json -> 이렇게 된 데이터를 분석해서 가져올 수 있음.
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://ckguscjf:113017@boilerplate.p7dht.mongodb.net/boilerplate?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  });
+
+  
 })
 
 app.listen(port, () => {
