1c649136489370229887f0a08daec04efa6c478b
qf9ar8nv <ckguscjf0@gmail.com>
2021-03-12 19:41:38 +0900

    Add userSchema, bodyParser
    * 로그인에 사용되는 mongoDB내 Schema작성법을 배움.
    * bodyParser를 통해 데이터 전달을 받음.

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 3000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://ckguscjf:Ck123456@cluster0.ws8sz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
@@ -13,6 +21,26 @@ mongoose.connect('mongodb+srv://ckguscjf:Ck123456@cluster0.ws8sz.mongodb.net/myF
 app.get('/', (req, res) => {
   res.send('Hello World!안녕하세요')
 })
+
+app.post('/register', (req, res) => {
+
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //데이터 베이스에 넣어주면 됨.
+
+  const user = new User(req.body)
+
+  
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
+
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
