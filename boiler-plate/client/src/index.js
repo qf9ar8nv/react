1000aa5cc6f578e01a0e11c3093fb532900c9e77
qf9ar8nv <ckguscjf0@gmail.com>
2021-03-16 23:52:13 +0900

    Add Redux, Antd
    * css framework인 Antd를 설치.
    * Redux에 대한 공부
    * Redux 설정.

--- boiler-plate/client/src/index.js
+++ boiler-plate/client/src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
+
 
 ReactDOM.render(
-  <React.StrictMode>
+
+  <Provider
+    store = {createStoreWithMiddleware()}
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>
+  
+  , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
